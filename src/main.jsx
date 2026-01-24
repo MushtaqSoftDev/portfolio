3bdaa3216149bd3cc4dd1bd1d104a4d70a75dbe5
ahmad <ahmad@ahmad.ahmad>
2026-01-24 17:39:58 +0100

    disable webgpu before app load

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+if (typeof navigator !== "undefined") {
+  navigator.gpu = undefined;
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
